956a057de7008a2c0de957a5bc0e23d87847631b
Costa-Stavrianidis <87909649+Costa-Stavrianidis@users.noreply.github.com>
2022-04-25 15:50:19 -0400

    Added optimization function
    Added optimization function to restaurant_functions.py file, consolidated parsing functions into restaurant_functions.py, edited README.md, added tests for the optimization function

--- restaurant_functions.py
+++ restaurant_functions.py
@@ -1,15 +1,87 @@
-"""
-Create restaurant functions to be imported by user.
-"""
+"""Create restaurant profile and function to be imported by user."""
 
-from datetime import datetime
+import sqlite3
+import numpy as np
+
+con = sqlite3.connect("restaurant.db")
+
+
+def parse_durham_data(filename: str) -> None:
+    """Read the file and parse the data files into a SQLite table."""
+    cur = con.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS Durham([Name] TEXT PRIMARY KEY, [Latitude] FLOAT, [Longitude] FLOAT, [VeganFriendly] BINARY, [Delivery] BINARY, [Curbside] BINARY, [PetFriendly] BINARY, [LargeParty] BINARY, [Catering] BINARY, [CuisineType] TEXT, [RestaurantType] TEXT, [OpeningTime] TEXT, [ClosingTime] TEXT, [Rating] FLOAT, [PriceLevel] TEXT)"
+    )
+    with open(filename, encoding="utf-8-sig") as p:
+        lines = p.readlines()
+        col_name = []
+        first_row = True
+        for line in lines:
+            line = line.strip()
+            if first_row:
+                col_name = line.split()
+                first_row = False
+            elif not first_row:
+                dic = {}
+                dat = line.split("\t")
+                for count, ele in enumerate(dat, 0):
+                    dic[col_name[count]] = dat[count]
+                lst = [
+                    dic["Name"],
+                    dic["Latitude"],
+                    dic["Longitude"],
+                    dic["VeganFriendly"],
+                    dic["Delivery"],
+                    dic["Curbside"],
+                    dic["PetFriendly"],
+                    dic["LargeParty"],
+                    dic["Catering"],
+                    dic["CuisineType"],
+                    dic["RestaurantType"],
+                    dic["OpeningTime"],
+                    dic["ClosingTime"],
+                    dic["Rating"],
+                    dic["PriceLevel"],
+                ]
+                cur.execute(
+                    "INSERT or REPLACE INTO Durham VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    lst,
+                )
+    return
+
+
+def parse_comments_data(filename: str) -> None:
+    """Read the file and parse the data files into a SQLite table."""
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS Comments([Name] TEXT, [Comments] TEXT)")
+    with open(filename, encoding="utf-8-sig") as p:
+        lines = p.readlines()
+        col_name = []
+        first_row = True
+        for line in lines:
+            line = line.strip()
+            if first_row:
+                col_name = line.split()
+                first_row = False
+            elif not first_row:
+                dic = {}
+                dat = line.split("\t")
+                for count, ele in enumerate(dat, 0):
+                    dic[col_name[count]] = dat[count]
+                lst = [
+                    dic["Name"],
+                    dic["Comments"],
+                ]
+                cur.execute("INSERT or REPLACE INTO Comments VALUES (?, ?)", lst)
+    return
 
 
 class Customer:
     """
-    This class will store user preferences for restaurants
-    and be used in later functions to distinguish restaurant
-    profiles that match user preferences.
+    This class will store user preferences for restaurants and
+    be used in the restaurant_ranker function to rank restaurants
+    in Durham that best fit the user's preferences.
+
     """
 
     def __init__(
@@ -17,10 +89,10 @@ class Customer:
         Name: str,
         Latitude: float,
         Longitude: float,
-        PriceLevel: list[str],
+        PriceLevel: str,
         OpeningTime: str,
         ClosingTime: str,
-        CuisineType: str,
+        CuisineType: list[str],
         RestaurantType: str,
         VeganFriendly: str,
         Delivery: str,
@@ -46,3 +118,121 @@ class Customer:
         self.PetFriendly = PetFriendly
         self.Catering = Catering
         self.Reviews = Reviews
+
+
+def restaurant_ranker(user: Customer) -> list[str]:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Durham")
+    restaurant_table = cur.fetchall()
+    restaurant_list = []
+    for restaurant in restaurant_table:
+        restaurant_sublist = [restaurant[0]]
+        score = 0
+        if user.VeganFriendly == "No":
+            if any(
+                hour
+                in list(
+                    range(
+                        int(restaurant[11][:2] + restaurant[11][3:]),
+                        int(restaurant[12][:2] + restaurant[12][3:]),
+                    )
+                )
+                for hour in list(
+                    range(
+                        int(user.OpeningTime[:2] + user.OpeningTime[3:]),
+                        int(user.ClosingTime[:2] + user.ClosingTime[3:]),
+                    )
+                )
+            ):
+                if restaurant[9] in user.CuisineType:
+                    score += 20
+                if user.LargeParty == "Yes" and restaurant[7] == 1:
+                    score += 20
+                if user.RestaurantType == restaurant[10]:
+                    score += 10
+                if user.Delivery == "Yes" and restaurant[4] == 1:
+                    score += 10
+                if user.Catering == "Yes" and restaurant[8] == 1:
+                    score += 10
+                if user.PetFriendly == "Yes" and restaurant[6] == 1:
+                    score += 5
+                if restaurant[13] in np.arange(
+                    user.Reviews - 0.5, user.Reviews + 0.5, 0.1
+                ):
+                    score += 5
+                elif restaurant[13] in np.arange(
+                    user.Reviews - 1.0, user.Reviews + 1.0, 0.1
+                ):
+                    score += 3
+                if len(user.PriceLevel) == len(restaurant[14]):
+                    score += 5
+                elif (
+                    len(restaurant[12]) == len(user.PriceLevel) - 1
+                    or len(user.PriceLevel) + 1
+                ):
+                    score += 3
+                restaurant_sublist.append(score)
+            else:
+                restaurant_sublist.append(score)
+        elif user.VeganFriendly == "Yes" and restaurant[3] == 1:
+            if any(
+                hour
+                in list(
+                    range(
+                        int(restaurant[11][:2] + restaurant[11][3:]),
+                        int(restaurant[12][:2] + restaurant[12][3:]),
+                    )
+                )
+                for hour in list(
+                    range(
+                        int(user.OpeningTime[:2] + user.OpeningTime[3:]),
+                        int(user.ClosingTime[:2] + user.ClosingTime[3:]),
+                    )
+                )
+            ):
+                if restaurant[9] in user.CuisineType:
+                    score += 20
+                if user.LargeParty == "Yes" and restaurant[7] == 1:
+                    score += 20
+                if user.RestaurantType == restaurant[10]:
+                    score += 10
+                if user.Delivery == "Yes" and restaurant[4] == 1:
+                    score += 10
+                if user.Catering == "Yes" and restaurant[8] == 1:
+                    score += 10
+                if user.PetFriendly == "Yes" and restaurant[6] == 1:
+                    score += 5
+                if restaurant[13] in np.arange(
+                    user.Reviews - 0.5, user.Reviews + 0.5, 0.1
+                ):
+                    score += 5
+                elif restaurant[13] in np.arange(
+                    user.Reviews - 1.0, user.Reviews + 1.0, 0.1
+                ):
+                    score += 3
+                if len(user.PriceLevel) == len(restaurant[14]):
+                    score += 5
+                elif (
+                    len(restaurant[12]) == len(user.PriceLevel) - 1
+                    or len(user.PriceLevel) + 1
+                ):
+                    score += 3
+                restaurant_sublist.append(score)
+            else:
+                restaurant_sublist.append(score)
+        elif user.VeganFriendly == "Yes" and restaurant[3] == 0:
+            restaurant_sublist.append(score)
+        distance = np.linalg.norm(
+            np.array((restaurant[1], restaurant[2]))
+            - np.array((user.Latitude, user.Longitude))
+        )
+        restaurant_sublist.append(distance)
+        restaurant_list.append(restaurant_sublist)
+    restaurant_list.sort(key=lambda x: x[2])
+    for restaurants in restaurant_list:
+        restaurants[1] += 15 - restaurant_list.index(restaurants)
+    restaurant_list.sort(key=lambda x: x[1], reverse=True)
+    restaurant_ordered_list = []
+    for ordered_restaurants in restaurant_list:
+        restaurant_ordered_list.append(ordered_restaurants[0])
+    return restaurant_ordered_list
--- user_instructions.py
+++ user_instructions.py
@@ -5,13 +5,21 @@ function will be described in detail below.
 
 """
 
-from restaurant_functions import Customer
+from restaurant_functions import (
+    parse_durham_data,
+    parse_comments_data,
+    Customer,
+    restaurant_ranker,
+)
+
+parse_durham_data("Durham")
+parse_comments_data("Comments")
 
 # Create your restaurant profile
 """
-In order to have the best experience with our restaurant locator function,
-users should follow the directions below as exactly as possible. This includes
-following case sensitve actions and numeric inputs. Users may follow the outlined
+In order to be able to successfully use our restaurant ranking function,
+users must follow the directions below exactly as they are stated. This includes
+following case sensitive actions and numeric inputs. Users may follow the outlined
 example below for reference.
 
 Step 1 - Give your Profile a name
@@ -33,11 +41,11 @@ Longitude: Obtain the exact longitude of your location. Users will input this va
 any and all decimal places present in their longitude. This value will be paired with the latitude
 input and used to find the restaurant closest to you that meets your profile criteria.
 
-PriceLevel: Indicated on a scale denoted by $, $$, $$$, $$$$ where the greater number of $
+Price Level: Indicated on a scale denoted by $, $$, $$$, $$$$ where the greater number of $
 implies that the desired price level of the restaurant is higher. This should be between
 quotations as such, "$$".
 
-OpeningTime: Using a 24:00 hour clock, users will input the opening hours of which they are
+Opening Time: Using a 24:00 hour clock, users will input the opening hours of which they are
 interested in visiting the restaurant so that the function can select restaurants which
 operate within the desired duration. For example, 6:45pm would be entered as "18:45".
 If a restaurant does not operate during the requested hours, it will not be suggested to
@@ -51,8 +59,12 @@ operate within the desired duration. This value will be paired with OpeningTime 
 duration of hours the user is interested in. This input should be between quotations, as such:
 "18:45".
 
-RestaurantType: Indicate the type of restaurant is preferred. This preferrence should
-match exactly to one of the following options and should be between quotations:
+Cuisine Type: Use a list to indicate the cuisine type you prefer for your dining experience.
+An example could be: ["American", "Chinese"].
+
+RestaurantType: Indicate the type of restaurant dining experience that is preferred. 
+This preference should match exactly to one of the following options and should be 
+between quotations:
     "Sit down", "Cafe", "Fast Food"
 
 VeganFriendly: Users may indicate if they require a restaurant with vegan options.
@@ -72,17 +84,18 @@ Catering - Users looking for catering options should input "Yes"/, otherwise "No
 Reviews - The review option allows users to input the restaurant standard that they
 are looking for. On a scale of 1-5, users can designate if they are looking for a
 restaurant of quality 3/5.0 or 4.5/5.0 for example. The user need only input their
-desired quality level and may use up to one decimal of exactness (ex. 2.3, 4.6).
+desired quality level and must use up to one decimal of exactness (ex. 2.3, 4.6).
+
 """
 
 firstname_lastname = Customer(
     "Input Name",
-    36.00899920398807,
-    -78.94173551747592,
-    ["$$"],
+    36.0089992,
+    -78.9417355,
+    "$$",
     "08:30",
     "12:00",
-    "Asian",
+    ["Chinese", "Indian"],
     "Sit down",
     "Yes",
     "Yes",
@@ -90,5 +103,23 @@ firstname_lastname = Customer(
     "No",
     "No",
     "Yes",
-    1.5,
+    4.5,
 )
+
+# Use your restaurant profile to rank restaurants in Durham
+"""
+Here is where the restaurant_ranker function will take your profile you created
+above, called firstname_lastname, and use your preferences for a restaurant
+to rank all the available restaurants in Durham that are in our database. This
+will output an ordered list with the first restaurant in the list being the one
+the function believes you will have the most optimal dining experience at, and 
+the last restaurant in the list being the one it believes you will have the least 
+optimal dining experience at.
+
+To receive your list of ordered restaurants, simply fill out your restaurant 
+profile as a Customer class above, replace the name of your Customer class as the 
+input of the restaurant_ranker function below, save, and run this Python script.
+
+"""
+
+print(restaurant_ranker(firstname_lastname))
